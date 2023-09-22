c03b7963ac6379b4da46a197d3dffcfd79d6250d
Estudante <rpcconcursos@gmail.com>
2023-09-22 00:45:04 -0300

    Exercício: Gerenciamento de Produtos em um Carrinho de Compras
    Descrição:
    Neste exercício, você criará um programa simples para gerenciar produtos em um carrinho de compras usando conjuntos (Sets) em Kotlin. O objetivo é permitir ao usuário adicionar, remover e listar produtos no carrinho.

---
+++ desafio_2_conjunto.kt
@@ -0,0 +1,74 @@
+fun main() {
+    selecionarProduto()
+}
+
+fun selecionarProduto() {
+    val carrinho = mutableSetOf<String>()
+
+    println("Bem-vindo ao gerenciamento de carrinho de compras!")
+    println("Você pode adicionar, remover e listar produtos no carrinho.")
+
+    while (true) {
+        println(
+            "Opções:\n" +
+                    "1. Adicionar produto ao carrinho\n" +
+                    "2. Remover produto do carrinho\n" +
+                    "3. Listar produtos no carrinho\n" +
+                    "4. Sair do programa"
+        )
+
+        print("Escolha uma opção: ")
+        val escolha = readlnOrNull()?.toIntOrNull()
+
+        when (escolha) {
+            1 -> adiconarProduto(carrinho)
+            2 -> removerProdto(carrinho)
+            3 -> listarProduto(carrinho)
+            4 -> {
+                println("Programa encerrado.")
+                return
+            }
+
+            else -> {
+                println("Nome invalido. Tente novamente.")
+            }
+        }
+    }
+}
+
+fun adiconarProduto(carrinho: MutableSet<String>) {
+    print("Digite o nome do produto a ser adicionado: ")
+    val produto = lerProduto()
+    carrinho.add(produto)
+    println("'$produto' foi adicionado ao carrinho.")
+}
+
+fun removerProdto(carrinho: MutableSet<String>) {
+    print("Digite o nome do produto a ser removido: ")
+    val produto = lerProduto()
+    if (produto.isNotEmpty() && carrinho.remove(produto)) {
+        println("'$produto' foi removido do carrinho.")
+    } else {
+        println("'$produto' não foi encontrado no carrinho.")
+    }
+}
+
+fun listarProduto(carrinho: MutableSet<String>) {
+    if (carrinho.isEmpty()) {
+        println("O carrinho está vázio!")
+    } else {
+        carrinho.forEachIndexed { indice, item ->
+            println("${indice + 1}. $item")
+        }
+
+    }
+}
+
+fun lerProduto(): String {
+    var entradaProduto = readlnOrNull()?.trim()
+    while (entradaProduto.isNullOrEmpty()) {
+        println("nome invalido! Tente novamente.")
+        entradaProduto = readlnOrNull()?.trim()
+    }
+    return entradaProduto
+}
