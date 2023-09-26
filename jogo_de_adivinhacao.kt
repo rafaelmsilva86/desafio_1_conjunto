9be33486fc7d1d85a4a8dc14bea10959d3d7a9cc
Estudante <rpcconcursos@gmail.com>
2023-09-26 15:08:06 -0300

    Desafio: "Jogo de Adivinhação de Números Únicos"
    Descrição:
    Neste desafio, você criará um jogo de adivinhação de números únicos usando conjuntos (Sets) em Kotlin. O objetivo do jogo é que o jogador adivinhe um número secreto que está dentro de um intervalo específico. O jogador pode fazer várias tentativas, mas não deve repetir os números já adivinhados.

--- jogo_de_adivinhacao.kt
+++ jogo_de_adivinhacao.kt
@@ -1,17 +1,29 @@
 fun main() {
+    escolherPalpites()
+}
 
+fun escolherPalpites() {
     val numeroAdivinhados = mutableSetOf<Int>()
-    val numerosAleatorios = (1..10).random()
+    val numerosAleatorios = numeroAleatorios()
     val intervalos = 1..10
     var tentativas = 0
-
+    println(
+        "------------------------------------------\n" +
+                "Vamos jogar ?\n" +
+                "Descubra o número de 1 a 10 se for capaz!\n" +
+                "------------------------------------------"
+    )
     while (true) {
+
         println("Adivinhe um número:")
         val palpite = readlnOrNull()?.toIntOrNull()
         if (palpite != null && palpite in intervalos) {
             when (palpite) {
                 numerosAleatorios -> {
-                    println("Você acertou!")
+                    println(
+                        "P a r a b é n s!!!\n" +
+                                " Você acertou!"
+                    )
                     return
                 }
 
@@ -25,12 +37,13 @@ fun main() {
                     val limiteTentativas = 3
                     println("Números de tentativas: $tentativas de $limiteTentativas")
 
-                    val dica = if (tentativas == limiteTentativas) "Maior" else "Menor"
+                    val dica = if (tentativas >= limiteTentativas) "Maior" else "Menor"
                     println("Dica: o número é $dica que o seu palpite, $palpite")
 
                     if (tentativas == limiteTentativas) {
                         println(
-                            "Números de tentativas esgotadas. O número era $numerosAleatorios.\n" +
+                            "Números de tentativas esgotadas.\n" +
+                                    " O número era $numerosAleatorios.\n" +
                                     "Por favor, reinicie o jogo para jogar novamente."
                         )
                     }
@@ -38,6 +51,8 @@ fun main() {
             }
         }
     }
+}
 
-
+fun numeroAleatorios(): Int {
+    return (1..10).random()
 }
